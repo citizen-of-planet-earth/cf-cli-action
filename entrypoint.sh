045154c5e7ee754f35546e76cadfc8e1c8ecb8d1
Kellen Altenhofen <kellen@simple.com>
2020-11-23 13:59:57 -0800

    cf7 commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
-cf api "$INPUT_CF_API"
-cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
+cf7 api "$INPUT_CF_API"
+cf7 auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
-  cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
+  cf7 target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 fi
 
 sh -c "cf7 $*"
